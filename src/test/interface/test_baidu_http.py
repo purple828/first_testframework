888aa649b2f20cd0df8955ea0bdd5f569b5e4b3f
fanglijuan <944974531@qq.com>
2018-07-13 17:47:23 +0800

    断言

--- src/test/interface/test_baidu_http.py
+++ src/test/interface/test_baidu_http.py
@@ -3,6 +3,7 @@ from src.utils.config import Config,REPORT_PATH
 from src.utils.client import HTTPClient
 from src.utils.log import logger
 from src.utils.HTMLTestRunner import HTMLTestRunner
+from src.utils.assertion import assertHTTPCode
 
 class TestBaiDuHTTP(unittest.TestCase):
 
@@ -14,11 +15,12 @@ class TestBaiDuHTTP(unittest.TestCase):
     def test_baidu_http(self):
         res = self.client.send()
         logger.debug(res.text)
+        assertHTTPCode(res,[400])
         self.assertIn('百度一下，你就知道',res.text)
 
 
 if __name__ == 'test_baidu_http':
-    report = REPORT_PATH + r'report_interface.html'
+    report = REPORT_PATH + r'\report_interface.html'
     with open(report,'wb') as f:
         runner = HTMLTestRunner(f,verbosity=2,title='接口测试报告',description='接口html报告')
         runner.run(TestBaiDuHTTP('test_baidu_http'))
